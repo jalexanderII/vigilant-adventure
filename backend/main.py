6dc0506041b81bb821a397564e8e87ec913955d8
ella511 <74123972+ella511@users.noreply.github.com>
2022-12-27 13:48:45 -0500

    main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,36 @@
-def hello_world():
-    return 'Hello World'
+from typing import List
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
+from datetime import datetime
+import json
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[""],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-if __name__ == '__main__':
-    print(hello_world())
+class ConnectionManager:
+    def __init__(self)->None:
+        self.active_connections: List[WebSocket] = []
+
+        async def connect(self, websocket: WebSocket):
+            await websocket.accept()
+            self.active_connections.append(websocket)
+
+            def disconnect(self, websocket:WebSocket):
+                self.active_connections.remove(websocket)
+
+                async def send_personal_message(self, message: str, websocket: WebSocket):
+                    await websocket.send_text(message)
+
+                    async def broadcast(self, message: str):
+                        for connection in self.active_connections:
+                            await connection.send_text(message)
+
+
