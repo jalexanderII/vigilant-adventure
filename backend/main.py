2fbadf3f29b401d8d4dd317fd24b7e998b541221
ella511 <74123972+ella511@users.noreply.github.com>
2022-12-27 14:05:53 -0500

    main.py

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from typing import List
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime
 import json
@@ -19,19 +19,19 @@ class ConnectionManager:
     def __init__(self)->None:
         self.active_connections: List[WebSocket] = []
 
-        async def connect(self, websocket: WebSocket):
-            await websocket.accept()
-            self.active_connections.append(websocket)
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
 
-            def disconnect(self, websocket:WebSocket):
-                self.active_connections.remove(websocket)
+    def disconnect(self, websocket:WebSocket):
+        self.active_connections.remove(websocket)
 
-                async def send_personal_message(self, message: str, websocket: WebSocket):
-                    await websocket.send_text(message)
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
 
-                    async def broadcast(self, message: str):
-                        for connection in self.active_connections:
-                            await connection.send_text(message)
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
 
 
 
@@ -42,21 +42,21 @@ manager = ConnectionManager()
 def Home():
      return "Welcome home"
 
-     @app.websocket("/ws/ {client_id}")
-     async def websocket_endpoint(websocket: WebSocket, client_id: int):
-        await manager.connect(websocket)
-        now = datetime.now() 
-        current_time = now.strftime("%H:%M")
-        
-        
-        try:
-            while True:
-                data = await websocket.receive_text()
-                message = {"time": currentcy_time, "client": client_id, "message": data}
-                await managers.broadcast(json.dumps(message))
-        except WebSocketDisconnect:
-            manager.disconnect(websocket)
-            message = {"time": current_time, "client_id":client_id, "message": "Offline"}
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    now = datetime.now() 
+    current_time = now.strftime("%H:%M")
+
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+            message = {"time": current_time, "client": client_id, "message": data}
             await manager.broadcast(json.dumps(message))
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        message = {"time": current_time, "client_id":client_id, "message": "Offline"}
+        await manager.broadcast(json.dumps(message))
 
 
